ca6145d366a21d0973ee3a8dc14f119054299243
eaglesareedible <zadler2029@kehillahstudent.org>
2026-05-01 11:08:03 -0700

    Proposal

--- .github/py.py
+++ .github/py.py
@@ -1,7 +1,8 @@
 import pygame as py
 from pygame.locals import *
 import sys
-import random
+import random, time
+import numpy
 
 py.init()
  
@@ -19,7 +20,7 @@ WHITE = (255, 255, 255)
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 300
 
-displayscreen = py.display.set_mode((300, 300), flags = py.RESIZABLE)
+displayscreen = py.display.set_mode((320, 320), flags = py.RESIZABLE)
 displayscreen.fill(WHITE)
 py.display.set_caption("Game")
 
@@ -31,15 +32,17 @@ print(object1.colliderect(object2))
 FPS = py.time.Clock()
 FPS.tick(66)
 py.init()
+scalefactor = 20/66
 
-
+apple = [60, 60]
 class Player(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = py.image.load("Player.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (150, 150)
+        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
         self.direction = (0, 0)
+        self.position = numpy.array([6, 8])
     def update(self):
         pressed_keys = py.key.get_pressed()
         if pressed_keys[K_UP]:
@@ -56,8 +59,9 @@ class Player(py.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 if self.direction != (-1, 0):
                     self.direction = (1, 0)
-        self.rect.move_ip(self.direction[0], self.direction[1])
-
+        self.position += numpy.array(self.direction)
+        self.rect.center = (int(self.position[0])*scalefactor, int(self.position[1])*scalefactor)
+        #self.rect.move_ip(int(self.position[0])*scalefactor, int(self.position[1])*scalefactor)
     def draw(self, surface):
         surface.blit(self.image, self.rect)
     def move(self):
@@ -70,13 +74,20 @@ class Apple(py.sprite.Sprite):
         super().__init__() 
         self.image = py.image.load("Apple.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
+        self.rect.center = (random.randint(40,SCREEN_WIDTH-40), random.randint(40,SCREEN_WIDTH-40))
     def draw(self, surface):
         surface.blit(self.image, self.rect)   
 
 P1 = Player()
 A1 = Apple()
+apples = py.sprite.Group()
+apples.add(A1)
+all_sprites = py.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(A1)
+
 while True:
+    count = 0
     for event in py.event.get():
         if event.type == QUIT:
             py.quit()
@@ -85,4 +96,9 @@ while True:
     P1.update()
     P1.draw(displayscreen)
     A1.draw(displayscreen)
+    if py.sprite.spritecollideany(P1, apples):
+        py.display.update()
+        A1 = Apple()
+        count +=1
+
     py.display.update()
---
+++ Proposal.md
@@ -0,0 +1,3 @@
+I want to create a snake game
+
+The smallest piece I've already started doing is the movement, but I don't have the tools for animations if I want them, or lengthening the snake yet. 
