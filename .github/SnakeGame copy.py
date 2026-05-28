f70aaf74d3b1672cd8a3c7f12fc4797f0e751a0c
eaglesareedible <zadler2029@kehillahstudent.org>
2026-05-28 08:40:21 -0700

    Final

--- .github/SnakeGame copy.py
+++ .github/SnakeGame copy.py
@@ -4,12 +4,21 @@ import sys
 import numpy
 import random
 py.init()
+
+taildic = {
+
+}
+
 scalefactor = 50
 Screen = py.display.set_mode([16*scalefactor, 16*scalefactor])
 playerimageright = py.image.load("Player-Right.png")
 playerimageright = py.transform.scale(playerimageright, (scalefactor, scalefactor)).convert_alpha()
 playerimageleft = py.image.load("Player-Left.png")
 playerimageleft = py.transform.scale(playerimageleft, (scalefactor, scalefactor)).convert_alpha()
+playerimagerighttail = py.image.load("Player-Tail-Right.png")
+playerimagerighttail = py.transform.scale(playerimagerighttail, (scalefactor, scalefactor)).convert_alpha()
+playerimagelefttail = py.image.load("Player-Tail-Left.png")
+playerimagelefttail = py.transform.scale(playerimagelefttail, (scalefactor, scalefactor)).convert_alpha()
 appleimage = py.image.load("Apple.png")
 appleimage = py.transform.scale(appleimage, (scalefactor, scalefactor))
 xd = 0
@@ -18,16 +27,41 @@ direction = ""
 position = (scalefactor, scalefactor)
 FPS = py.time.Clock()
 count = 0
+frame = 0
+history = [
+
+]
+historicaldirection = [
+
+]
+class Tail:
+    def __init__(self):
+        self.playerimagerighttail = py.image.load("Player-Tail-Right.png")
+        self.playerimagerighttail = py.transform.scale(playerimagerighttail, (scalefactor, scalefactor)).convert_alpha()
+        self.playerimagelefttail = py.image.load("Player-Tail-Left.png")
+        self.playerimagelefttail = py.transform.scale(playerimagelefttail, (scalefactor, scalefactor)).convert_alpha()
+    def draw(self, position, direction):
+        if direction == "right":
+            self.playerimagerighttail = py.image.load("Player-Tail-Right.png")
+            self.playerimagerighttail = py.transform.scale(playerimagerighttail, (scalefactor, scalefactor)).convert_alpha()
+            Screen.blit(playerimagerighttail, position)
+        elif direction == "left":
+            self.playerimagelefttail = py.image.load("Player-Tail-Left.png")
+            self.playerimagelefttail = py.transform.scale(playerimagelefttail, (scalefactor, scalefactor)).convert_alpha()
+            Screen.blit(playerimagelefttail, position)
+
 appleposition = (8*scalefactor, 8*scalefactor)
 def startApple():
     global appleposition 
     appleposition = (8*scalefactor, 8*scalefactor)
 def newApple():
     global appleposition 
-    appleposition = (random.randint(0, 16)*scalefactor, random.randint(0, 16)*scalefactor)
+    appleposition = (random.randint(0, 15)*scalefactor, random.randint(0, 15)*scalefactor)
+istail= False
+
 while True:
     recievedinput = False
-    FPS.tick(15)
+    FPS.tick(12)
     for event in py.event.get():
         if event.type == py.QUIT:
             py.quit()
@@ -70,9 +104,22 @@ while True:
     if position == appleposition:
         count += 1
         newApple()
+        while appleposition in taildic:
+            newApple()
+        taildic[count]= Tail()
+        istail = True
+    if istail == True:
+        for i in taildic:
+            taildic[i].draw(history[frame-i], historicaldirection[frame-i])
+            if history[frame-i] == position:
+                py.quit()
+                sys.exit()
     Screen.blit(appleimage, appleposition)
+    history.append(position)
+    historicaldirection.append(direction)
+    frame += 1
     if position[0]>=17*scalefactor or position[1]>=17*scalefactor or position[0] <= -1 or position[1] <= -1:
         py.quit()
         sys.exit()
-    print(count)
+
     py.display.flip()   
